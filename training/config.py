9f59a17e8dc9636695abc036a85456918afa143d
Jinwon Pyo <gch99517@gmail.com>
2025-12-30 21:57:17 +0900

    Use CPU only for now

--- training/config.py
+++ training/config.py
@@ -2,11 +2,12 @@ import torch
 
 class Config:
     def __init__(self):
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() 
-            else 'mps' if torch.backends.mps.is_available() 
-            else "cpu"
-        )
+        self.device = "cpu" # 현재는 CPU 고정
+        # self.device = torch.device(
+        #     "cuda" if torch.cuda.is_available() 
+        #     else 'mps' if torch.backends.mps.is_available() 
+        #     else "cpu"
+        # )
         self.label_path = "/app/data/raw/application_train.csv"
         self.num_epochs = 10
         self.lr = 1e-3
